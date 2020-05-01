5b86ecf5a116a814c8b378319d094d78bee180b9
Arul <aprprasanth16@gmail.com>
2020-05-01 14:56:34 -0400

    finalised

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -2,42 +2,65 @@ const express = require("express");
 const router = express.Router();
 const Article = require("../models/articleModel");
 
-router.get("", (req, res) => {
-  articles = [
-    {
-      title: "first title",
-      createdAt: new Date(),
-      description: "first description",
-    },
-    {
-      title: "2 title",
-      createdAt: new Date(),
-      description: "2 description",
-    },
-  ];
-
-  res.render("articles/index", { articles: articles });
-});
-router.get("/:id", (req, res) => {
-  res.render("articles/new");
-});
+//direct to create new article
 router.get("/new", (req, res) => {
-  res.render("articles/new");
+  res.render("articles/new", { article: new Article() });
 });
 
-router.post("/", (req, res) => {
-  const article = new Article({
-    title: req.body.title,
-    description: req.body.description,
-    markdown: req.body.markdown,
-  });
-  try {
-    articles.save();
-    res.redirect(`articles/${article.id}`);
-  } catch (err) {
-    console.log(err);
-    res.render("articles/new", { article: article });
-  }
+//edit form
+router.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("articles/edit", { article: article });
 });
+
+//save new article
+router.post(
+  "/",
+  async (req, res, next) => {
+    req.article = new Article();
+    next();
+  },
+  saveArticleAndRedirect("new")
+);
+
+//view a single article
+
+router.get("/:slug", async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
+
+  if (article == null) res.redirect("/");
+  res.render("articles/show", { article: article });
+});
+
+//edit
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect("edit")
+);
+
+//delete
+router.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+    try {
+      article = await article.save();
+      res.redirect(`/articles/${article.slug}`);
+    } catch (e) {
+      res.render(`articles/${path}`, { article: article });
+    }
+  };
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 require("dotenv").config();
 
 const express = require("express");
+const methodOverride = require("method-override");
 const connectDB = require("./config/databaseConnection");
 const app = express();
 const articleRoute = require("./routes/articleRoute");
-
 connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+const Article = require("./models/articleModel");
+
+//get all the articles
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+
 app.use("/articles", articleRoute);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
